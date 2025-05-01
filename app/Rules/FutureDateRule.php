3ec9105776798e1456d0a349b738ed56fc54a295
moner <monirkafori363@gmail.com>
2025-05-01 22:23:18 +0300

    custom rules

---
+++ app/Rules/FutureDateRule.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\Rule;
+
+class FutureDateRule implements Rule
+{
+
+   public function passes($attribute, $value)
+   {
+       return ($value && strtotime($value) <= time());
+   }
+
+    public function message(): string
+    {
+     return 'Future date is required';
+    }
+}
+
